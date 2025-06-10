88eab72742e8047710099ed1b08fef0d095c5957
Anatoli Yaschenko <yachenko.anatoliy37@gmail.com>
2025-06-10 10:58:51 +0300

    add task solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
+  extends: [
+    '@mate-academy/eslint-config',
+    'plugin:cypress/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'], // синхронізує з Prettier
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'all',
+        tabWidth: 2,
+        printWidth: 80,
+      },
+    ],
+  },
 };
